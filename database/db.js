262b6a2018b2bdb09409d4d104f6707842cf57b1
pandubhaskara <pandubhaskara40@gmail.com>
2021-11-21 18:10:17 +0700

    procfile

--- database/db.js
+++ database/db.js
@@ -1,10 +1,11 @@
-require('dotenv').config()
-
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 const mongoose = require("mongoose");
+
 module.exports = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {});
-    console.log("connected to MongoDB");
+    console.log("Connected to MongoDB");
   } catch (error) {
     console.log(error);
   }
