11fe1718617392e712f742d6070c784dc26caa84
pandubhaskara <pandubhaskara40@gmail.com>
2021-11-21 18:15:47 +0700

    procfile

--- database/db.js
+++ database/db.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 module.exports = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {});
+    await mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log(error);
