0d18eadf3374920714ecd1681a700e7b022b2d23
pandubhaskara <pandubhaskara40@gmail.com>
2021-11-21 18:23:14 +0700

    procfile

--- database/db.js
+++ database/db.js
@@ -1,10 +1,13 @@
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, './.env') });
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 const mongoose = require("mongoose");
 
 module.exports = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
+    await mongoose.connect(
+      process.env.MONGO_URI || "mongodb://localhost/warehoueseapi",
+      { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+    );
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log(error);
