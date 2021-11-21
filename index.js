f729fd89563ebafeb4ba7ea9393e84b0b10e39eb
pandubhaskara <pandubhaskara40@gmail.com>
2021-11-21 18:43:27 +0700

    procfile

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 5000 || process.env.PORT
-const db = require('./database/db')
-const route = require('./routes')
-const host = '0.0.0.0';
-require('dotenv').config()
-db()
+const express = require("express");
+const app = express();
+const port = 5000 || process.env.PORT;
+const db = require("./database/db");
+const route = require("./routes");
+const host = "0.0.0.0";
+require("dotenv").config();
+db();
 
-app.get("/",(req,res)=>{
-    res.status(200).send({
-        status: "success",
-        message:"API is online"
-    })
-})
-app.use(express.json())
-app.use('/api/v1', route)
+app.get("/", (req, res) => {
+  res.status(200).send({
+    status: "success",
+    message: "API is online",
+  });
+});
+app.use(express.json());
+app.use("/api/v1", route);
 
-app.listen(port,host, ()=>{
-    console.log(`server started at port ${port}`)
+app.listen(port, host, () => {
+  console.log(`server started at port ${port}`);
 });
