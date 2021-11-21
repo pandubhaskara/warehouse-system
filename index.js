662bc8966605d8ef46c74151a67481cc2226f86f
pandubhaskara <pandubhaskara40@gmail.com>
2021-11-21 18:54:50 +0700

    procfile

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 5000 || process.env.PORT;
+const port = process.env.PORT || 5000;
 const db = require("./database/db");
 const route = require("./routes");
 const host = "0.0.0.0";
