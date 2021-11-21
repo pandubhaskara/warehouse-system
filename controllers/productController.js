d7c3dadac4045e4cf9c9f1d194682bb834131afd
pandubhaskara <pandubhaskara40@gmail.com>
2021-11-21 16:03:16 +0700

    prodcut controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -21,10 +21,71 @@ module.exports = {
   readAll: async (req, res) => {
     const body = req.body;
     try {
-        const data= await product.find(body).populate('category')
+        const data= await product.find(body)
+        if(data.length==0){
+          return res.status(500).json({
+            status: "failed",
+            message : "there is no data"
+          })
+        }
         return res.status(200).json({
             status :'success',
             data : data
+        })
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        status: "error",
+        message: "internal server error",
+      });
+    }
+  },
+  readOne: async (req, res) => {
+    const id = req.params.id
+    try {
+        const data= await product.findById(id).populate('category', ["name"])
+        return res.status(200).json({
+            status :'success',
+            data : data
+        })
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        status: "error",
+        message: "internal server error",
+      });
+    }
+  },
+  update: async (req, res) => {
+    const body = req.body;
+    const name = req.params.id
+    try {
+        const updateProduct = await product.findOneAndUpdate({name:name}, 
+            body, {returnOriginal:false}).populate("category", ["name"]);
+        return res.status(200).json({
+            status :'success',
+            data : updateProduct
+        })
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        status: "error",
+        message: "internal server error",
+      });
+    }
+  },
+  delete: async (req, res) => {
+    const id = req.params.id;
+    try {
+        const deleteProduct = await product.deleteOne({name:id})
+        if(!deleteProduct.deletedCount){
+            return res.status(404).json({
+                status :"failed",
+                message : "the data not found"
+            })
+        }return res.status(400).json({
+            status: "success",
+            message:"data deleted successfully"
         })
     } catch (error) {
       console.log(error);
--- routes/product.js
+++ routes/product.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const product = require("../controllers/productController");
-const multer  = require('multer')
-const upload = multer()
+const multer = require("multer");
+const upload = multer();
 
-router.post("/",  upload.any(), product.create);
-router.get("/",  upload.any(), product.readAll);
-// router.get("/:id", laptop.readOne);
-// router.put("/:id", laptop.update);
-// router.delete("/:id", laptop.delete);
+router.post("/", upload.any(), product.create);
+router.get("/", upload.any(), product.readAll);
+router.get("/:id", product.readOne);
+router.put("/:id", upload.any(), product.update);
+router.delete("/:id", product.delete);
 
 module.exports = router;
